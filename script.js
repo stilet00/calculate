f53e9ad181b01d8b4f0258970b182d0bfa74f245
stilet00 <safroninanton@gmail.com>
2020-12-18 12:01:46 +0200

    adding colors to buttons

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttonCollection = document.querySelectorAll('button');
+    const div = document.querySelector('.menu')
     function setButtonsId() {
         for (let i = 0; i < buttonCollection.length; i++) {
             buttonCollection[i].setAttribute('id', i);
         }
     }
+    function buttonPressed(event) {
+        if (event.target.tagName === 'BUTTON') {
+            for (let i = 0; i < buttonCollection.length; i++) {
+                buttonCollection[i].classList.remove(`button-active`);
+            }
+            event.target.classList.toggle('button-active');
+        }
+    }
     setButtonsId();
-    console.log(buttonCollection[0]);
+    div.addEventListener('click', buttonPressed);
+
 })
